6e1a015acbbbec03c85adae269e0d44d12a728a5
yigitcankaya94 <yigitcankaya94@gmail.com>
2016-12-18 21:57:12 +0000

    bug fixes

--- client_files/auxiliary.py
+++ client_files/auxiliary.py
@@ -22,7 +22,7 @@ def generate_message(appliance_id=None, msg_type=None, transaction_id=None, valu
 
 
 def read_config(config_path):
-	with open(CONFIG_FILE) as config_file:    
+	with open(config_path) as config_file:    
 		config = json.load(config_file)
 		
 def get_time():
